8e07c0471677b1e2da63ff2374c0befd245c64df
Jason Buberel <jason@buberel.org>
2015-03-01 08:17:58 -0800

    Initial skeleton
    No actual features yet.

---
+++ extract.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "log"
+  )
+
+func main() {
+  log.Printf("Beginning tweet extraction.\n")
+
+  log.Printf("Completing tweet extraction, found %v tags and %v mentions.", 0, 0)
+}
